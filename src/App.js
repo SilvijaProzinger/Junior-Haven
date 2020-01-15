bed3434cfb69901cbb0b7ba1949fcf2def04885f
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2020-01-15 15:42:30 +0100

    homepage design finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import mainimage from './images/mainimage.png';
+import blogone from './images/blogone.jpg';
+import blogtwo from './images/blogtwo.jpg';
+import blogthree from './images/blogthree.jpg';
 
 function App() {
   return (
@@ -25,9 +28,21 @@ function App() {
           <img src={mainimage} alt="central illustration"/>
         </div>
         <div className="new-blogs">
-          <div className="blog-entry"></div>
-          <div className="blog-entry"></div>
-          <div className="blog-entry"></div>
+          <div className="blog-entry">
+            <h3>Why React is the best thing a new developer can learn?</h3>
+          {/*credit to Kevin Ku on Pexels*/}
+            <img src={blogone} alt="coding"/> 
+          </div>
+          <div className="blog-entry">  
+            <h3>Angular Basics - Part II</h3>
+          {/*credit to Christina Morillo on Pexels*/}
+            <img src={blogtwo} alt="coding"/>
+          </div>
+          <div className="blog-entry">
+            <h3>10 SASS tips for a perfect portfolio</h3>
+          {/*credit to Tranmautritam on Pexels*/}
+            <img src={blogthree} alt="coding"/>
+          </div>
         </div>
       </div>
       <div className="footer">
